6cce694709c9b82b2970a87c5be1ac9c6cf1fa58
shch91 <shch91@126.com>
2018-08-02 20:55:55 +0800

    gfdgsd

--- src/main/java/com/ldy/shch91/controller/HelloController.java
+++ src/main/java/com/ldy/shch91/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.ldy.shch91.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.base.Strings;
 import com.ldy.shch91.daoentity.Actor;
 import com.ldy.shch91.mapper.ActorMapper;
 import com.ldy.shch91.util.readResource.ReadResource;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
+import java.util.Vector;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
@@ -51,6 +53,9 @@ public class HelloController {
     public String first() {
 
      //readResource.getResourceByClassOrClassLoader();
+        boolean fda= Strings.isNullOrEmpty("");
+
+        Vector ver=new Vector<String>();
         Actor   actor=actorMapper.select(32);
         logger.info(JSON.toJSONString(actor));
         redisTemplate.opsForValue().set("userToJson", JSON.toJSONString(actor));
