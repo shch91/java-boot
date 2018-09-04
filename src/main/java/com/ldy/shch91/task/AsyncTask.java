57d0eff48f150c46fa2031decf15839b47a8d649
shch91 <github2016>
2018-09-04 22:35:43 +0800

    gfdsd

--- src/main/java/com/ldy/shch91/task/AsyncTask.java
+++ src/main/java/com/ldy/shch91/task/AsyncTask.java
@@ -5,14 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
-
 import java.util.concurrent.Future;
 
 @Component
 public class AsyncTask {
 
     private  static final Logger logger= LoggerFactory.getLogger(AsyncTask.class);
-      
+
     @Async
     public Future<Boolean> dotask(){
 
