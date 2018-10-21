0479a6b8cf193b98c4d0b373f25dbf21bb4d87b1
shch91 <github2016>
2018-10-21 18:31:12 +0800

    fdsa

--- src/main/java/com/ldy/shch91/Shch91Application.java
+++ src/main/java/com/ldy/shch91/Shch91Application.java
@@ -1,13 +1,8 @@
 package com.ldy.shch91;
 
-import com.ldy.shch91.netty.DiscardServer;
-import com.ldy.shch91.netty.EchoClient;
-import com.ldy.shch91.netty.EchoServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
