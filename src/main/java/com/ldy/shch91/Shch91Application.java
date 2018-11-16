111118f3790dc2b93995467a29478d8c0d6621bf
shch91 <github2016>
2018-11-16 19:45:23 +0800

    jar conflict

--- src/main/java/com/ldy/shch91/Shch91Application.java
+++ src/main/java/com/ldy/shch91/Shch91Application.java
@@ -3,6 +3,7 @@ package com.ldy.shch91;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ImportResource(locations={"classpath:spring/spring.xml"})
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableAsync
 public class Shch91Application {
 
