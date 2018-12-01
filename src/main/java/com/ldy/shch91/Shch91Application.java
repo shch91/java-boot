f4fbd00f38719bdf57813ce20f83bde98ca31758
shch91 <github2016>
2018-12-01 09:37:46 +0800

    rewq

--- src/main/java/com/ldy/shch91/Shch91Application.java
+++ src/main/java/com/ldy/shch91/Shch91Application.java
@@ -3,9 +3,11 @@ package com.ldy.shch91;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @ImportResource(locations={"classpath:spring/spring.xml"})
 @SpringBootApplication
@@ -18,5 +20,11 @@ public class Shch91Application {
     }
 
 
+
+
+    @Bean
+    public RestTemplate fds() {
+        return new RestTemplate();
+    }
 
 }
