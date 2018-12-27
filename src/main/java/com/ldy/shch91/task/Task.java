7c85d61af28507dd969065b2084de521a4525d3a
shch91 <github2016>
2018-12-27 16:55:21 +0800

    conflict

--- src/main/java/com/ldy/shch91/task/Task.java
+++ src/main/java/com/ldy/shch91/task/Task.java
@@ -6,8 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 public class Task {
 
 
+
     private static final String str="dev";
 
     private static final String strstr="test";
+
+
+    private static final String str="sdfdsfdsfsdafdsaf";
 
 }
