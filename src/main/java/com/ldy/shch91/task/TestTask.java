262d9376ea5369dcc22bb0ab561b8e02f6854304
shch91 <github2016>
2018-12-27 16:46:13 +0800

    d&e

--- src/main/java/com/ldy/shch91/task/TestTask.java
+++ src/main/java/com/ldy/shch91/task/TestTask.java
@@ -1,6 +1,7 @@
 package com.ldy.shch91.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,8 +10,8 @@ public class TestTask {
 
 
     //定义每三秒执行任务
-    //@Scheduled(fixedRate = 3000)
-    //@Scheduled(cron="0 */1 * * * ?")
+    @Scheduled(fixedRate = 3000)
+    @Scheduled(cron="0 */1 * * * ?")
     public void reportCurrentTime() {
         log.info("task runed");
     }
