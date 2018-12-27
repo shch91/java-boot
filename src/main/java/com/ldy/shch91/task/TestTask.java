b8bce34974973cec7c758016ef05479a34e09e18
shch91 <github2016>
2018-12-27 13:50:36 +0800

    Revert "Merge branch 'dev' into test" revert dev merger This reverts commit baf4f5bff2a928f50122bd9c591ee967780267a3, reversing changes made to bccf3931d590e87d1cfce77a45d20892b38fd093.

--- src/main/java/com/ldy/shch91/task/TestTask.java
+++ src/main/java/com/ldy/shch91/task/TestTask.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestTask {
 
-  private static final String revert="dasfdsafdsas";
+
 
     //定义每三秒执行任务
     //@Scheduled(fixedRate = 3000)
