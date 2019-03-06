73252511751e4dff1de9984fee86b7e4a23a47ed
shch91 <github2016>
2019-03-06 23:23:18 +0800

    kafka

---
+++ src/main/java/com/ldy/shch91/kafka/MyMessageListener.java
@@ -0,0 +1,18 @@
+package com.ldy.shch91.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+
+@Slf4j
+public class MyMessageListener implements MessageListener<String, String> {
+  
+    @Override//此方法处理消息
+    public void onMessage(ConsumerRecord<String, String> data) {
+        String topic = data.topic();//消费的topic
+        log.info("-------------recieve message from {} topic-------------", topic);
+        log.info("partition:{}", String.valueOf(data.partition()));//消费的topic的分区
+        log.info("offset:{}", String.valueOf(data.offset()));//消费者的位置
+        log.info("get message from {} topic : {}", topic, data.value());//接收到的消息
+    }
+}
