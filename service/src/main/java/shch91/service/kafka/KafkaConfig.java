b247835904de511202d901d6001384cb87d8e917
shch91 <github2016>
2019-06-10 16:09:45 +0800

    trwet

--- service/src/main/java/shch91/service/kafka/KafkaConfig.java
+++ service/src/main/java/shch91/service/kafka/KafkaConfig.java
@@ -9,8 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.config.ContainerProperties;
+
 
 import java.util.HashMap;
 import java.util.Map;
