31ba91575a258e8d7d16ba59e200b7c179b93be8
shch91 <github2016>
2020-07-28 20:43:14 +0800

    【

--- app/src/main/java/shch91/app/Main.java
+++ app/src/main/java/shch91/app/Main.java
@@ -2,32 +2,34 @@ package shch91.app;
 
 public class Main {
     public static void main(String[] args) {
-   getAns(15);
+        getAns(90);
     }
 
-    public  static  void getAns(int ans){
+    public static void getAns(int ans) {
+        if (ans < 3) {
+            return;
+        }
 
-        int beg=1;
-        int sum=1;
-        int cur=1;
-        while(beg<=ans/2+1){
-            if(sum==ans){
-                for(int k=beg;k<=cur;k++){
-                    System.out.println(k+"  ");
+        int beg = 1;
+        int sum = 3;
+        int cur = 2;
+        while (beg <= ans / 2 + 1) {
+            if (sum == ans) {
+                for (int k = beg; k <= cur; k++) {
+                    System.out.print(k + "  ");
                 }
                 System.out.println();
-                sum-=beg;
+                sum -= beg;
                 beg++;
                 cur++;
-                sum+=cur;
-            }
-            else if(sum<ans){
+                sum += cur;
+            } else if (sum < ans) {
                 //和小于ans
                 cur++;
-                sum+=cur;
-            }else{
+                sum += cur;
+            } else {
                 //和大于ans
-                sum-=beg;
+                sum -= beg;
                 beg++;
             }
         }
