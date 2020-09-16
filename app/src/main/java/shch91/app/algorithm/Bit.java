9376602ef7863e1219df2f169966f627d9281404
shch91 <github2016>
2020-09-16 15:59:27 +0800

    q

--- app/src/main/java/shch91/app/algorithm/Bit.java
+++ app/src/main/java/shch91/app/algorithm/Bit.java
@@ -6,9 +6,8 @@ import java.util.Map;
 
 public class Bit {
 
-
     public int maxPoints(int[][] points) {
-        if(points.length==0){
+        if (points.length == 0) {
             return 0;
         }
         //点数量
@@ -25,7 +24,7 @@ public class Bit {
                     duplicate++;
                 } else {
                     String t = slope(p2, p1);
-                    map.put(t, map.containsKey(t)?map.get(t)+1:1);
+                    map.put(t, map.containsKey(t) ? map.get(t) + 1 : 1);
                     localMax = Math.max(map.get(t), localMax);
                 }
             }
@@ -35,6 +34,13 @@ public class Bit {
         return max;
     }
 
+    /**
+     * 直线斜率
+     *
+     * @param p2
+     * @param p1
+     * @return
+     */
     private String slope(int[] p2, int[] p1) {
         String sign = sign(p2[0] - p1[0], p2[1] - p1[1]);
         int g = gcd(Math.abs(p2[0] - p1[0]), Math.abs(p2[1] - p1[1]));
@@ -63,6 +69,49 @@ public class Bit {
         }
     }
 
+    /**
+     * 分数转小数
+     *
+     * @param numerator   分子
+     * @param denominator 分母
+     * @return
+     */
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
+        //结果符号
+        if (numerator < 0 ^ denominator < 0) {
+            sb.append("-");
+        }
+        long chuShu = Math.abs(Long.valueOf(denominator));
+        long beiChuShu = Math.abs(Long.valueOf(numerator));
+        long shang = beiChuShu / chuShu, yuShu = beiChuShu % chuShu;
+        sb.append(shang);
+        if (yuShu == 0) {
+            return sb.toString();
+        }
+        sb.append(".");
+        //小数部分
+        while (yuShu > 0) {
+            //已经出现过改余数
+            if (map.containsKey(yuShu)) {
+                sb.insert(map.get(yuShu), "(");
+                sb.append(")");
+                return sb.toString();
+            }
+            map.put(yuShu, sb.length());
+            beiChuShu = yuShu * 10;
+            sb.append(beiChuShu / chuShu);
+            yuShu = beiChuShu % chuShu;
+        }
+        return sb.toString();
+    }
+
+
+
     public static void main(String[] args) {
         int[][] p = new int[][]{
                 {1, 1},
@@ -70,7 +119,9 @@ public class Bit {
                 {3, 3}
         };
         Bit b = new Bit();
-        int t = b.maxPoints(p);
-        System.out.println(t);
+        //int t = b.maxPoints(p);
+     //   System.out.println(Integer.MAX_VALUE);
+        System.out.println(1.0/998);
+        System.out.println(b.fractionToDecimal(1, 998));
     }
 }
