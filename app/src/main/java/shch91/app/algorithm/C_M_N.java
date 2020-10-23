ed118962738f076d4a2fd19121c006d99fe5277c
shch91 <github2016>
2020-10-23 21:46:40 +0800

    e

--- app/src/main/java/shch91/app/algorithm/C_M_N.java
+++ app/src/main/java/shch91/app/algorithm/C_M_N.java
@@ -1,13 +1,13 @@
 package shch91.app.algorithm;
 
 public class C_M_N {
-    static int N = 5;
-    static int M = 3;
+    static int M = 5;
+    static int N= 3;
     static int[] a= new int[]{1,2,3,4,5};
-    static int[] b = new int[M];
+    static int[] b = new int[N];
  
     public static void main(String[] args){
-        C(N,M);
+        C(M,N);
     }
 
     /**
@@ -24,7 +24,7 @@ public class C_M_N {
                 C(i - 1, n - 1);
             }
             else {
-                for(int j=0;j<=M-1;j++) {
+                for(int j=0;j<=N-1;j++) {
                     System.out.print(a[b[j]] + "  ");
                 }
                 System.out.println();
