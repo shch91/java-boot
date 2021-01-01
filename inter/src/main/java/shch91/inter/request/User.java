50fe635c610954b462e0dbe504c3a7eac5d25353
shch <shch91@126.com>
2021-01-01 21:32:35 +0800

    1

--- inter/src/main/java/shch91/inter/request/User.java
+++ inter/src/main/java/shch91/inter/request/User.java
@@ -1,12 +1,11 @@
 package shch91.inter.request;
 
+import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
-
-@Setter
 @Getter
+@Setter
 public class User implements Serializable {
     private static final long serialVersionUID = -8925536505890540756L;
 
